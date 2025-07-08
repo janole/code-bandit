c3c90852dd6c5c51d9a26f492c11ca4c44d47de3
janole <ole@mobileways.de>
2025-07-08 17:37:11 +0200

    feat: add watcher to redraw on terminal resize

---
+++ source/useTerminalSize.ts
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+
+export default function useTerminalSize()
+{
+    const [size, setSize] = useState({
+        columns: process.stdout.columns || 80,
+        rows: process.stdout.rows || 25,
+    });
+
+    useEffect(() =>
+    {
+        const handleResize = () =>
+        {
+            setSize({
+                columns: process.stdout.columns,
+                rows: process.stdout.rows,
+            });
+        };
+
+        process.stdout.on('resize', handleResize);
+
+        return () => { process.stdout.off('resize', handleResize); };
+    }, [
+        setSize,
+    ]);
+
+    return size;
+}

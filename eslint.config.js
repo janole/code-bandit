60f733ca778918fb0bc6928c386625643c667305
janole <ole@mobileways.de>
2025-07-09 14:41:50 +0200

    refactor: update eslint.config.js to use new parser and improve stylistic rules configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,34 +1,28 @@
+import tsParser from "@typescript-eslint/parser";
 import globals from "globals";
-import stylistic from "@stylistic/eslint-plugin";
 import eslintPluginUnicorn from "eslint-plugin-unicorn";
+import stylistic from "@stylistic/eslint-plugin";
 
 export default [
     {
-        ...stylistic.configs.customize({
-            indent: 4,
-            braceStyle: "allman",
-            quotes: "double",
-            semi: true,
-            jsx: true,
-            // ...
-        }),
         files: ["source/**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
-    },
-    {
-        files: ["source/**/*"],
         languageOptions: {
-            globals: globals.builtin,
+            parser: tsParser,
+            parserOptions: {
+                ecmaFeatures: { jsx: true },
+                ecmaVersion: 2020,
+                sourceType: "module",
+            },
+            globals: globals.node,
         },
         plugins: {
             unicorn: eslintPluginUnicorn,
+            "@stylistic": stylistic
         },
         rules: {
-            "unicorn/filename-case": [
-                "error",
-                {
-                    "case": "kebabCase"
-                }
-            ],
+            "unicorn/filename-case": ["error", { case: "kebabCase" }],
+            "@stylistic/brace-style": [2, "allman", { allowSingleLine: true }],
+            "@stylistic/quotes": [2, "double"]
         },
     },
 ];

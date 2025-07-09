43ffbdc7f094d56e36287840a3193d79a77f3e25
janole <ole@mobileways.de>
2025-07-09 14:42:42 +0200

    style: replace single quotes with double quotes for consistency in source/coba.tsx

--- source/coba.tsx
+++ source/coba.tsx
@@ -1,18 +1,18 @@
 #!/usr/bin/env node
-import { Command } from 'commander';
-import React from 'react';
-import { render } from 'ink';
-import App from './app.js';
-import path from 'path';
-import { cwd } from 'process';
+import { Command } from "commander";
+import React from "react";
+import { render } from "ink";
+import App from "./app.js";
+import path from "path";
+import { cwd } from "process";
 
 const program = new Command();
 
 program
-	.name('coba')
-	.description('Code Bandit')
-	.version('0.0.1')
-	.argument('[git-repo-path]', 'git repository directory', '.')
+	.name("coba")
+	.description("Code Bandit")
+	.version("0.0.1")
+	.argument("[git-repo-path]", "git repository directory", ".")
 	.option("-m, --model <model>", "Specify the model to be used", "magistral:24b")
 	.option("-p, --provider <provider>", "Specify the model provider to be used", "ollama")
 	.action(async (gitRepoPath: string, options) =>

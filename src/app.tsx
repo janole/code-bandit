f3bc2d0c8227e72f79f95b504a68d02ac8690338
janole <ole@mobileways.de>
2025-07-10 19:48:14 +0200

    refactor: simplify and modularize src/app.tsx by removing unused imports, inlining components

--- src/app.tsx
+++ src/app.tsx
@@ -1,84 +1,11 @@
-import React, { memo, useState } from "react";
-import { Box, Newline, Static, Text } from "ink";
+import React, { useState } from "react";
+import { Box, Static, Text } from "ink";
 import TextInput from "ink-text-input";
+import { HumanMessage } from "@langchain/core/messages";
 import { TMessage, work } from "./ai/work.js";
-import { AIMessage, HumanMessage, ToolMessage } from "@langchain/core/messages";
 import useTerminalSize from "./utils/use-terminal-size.js";
-import { ToolCall } from "@langchain/core/messages/tool";
-
-const colors = {
-	human: "green",
-	ai: "black",
-	generic: "black",
-	tool: "red",
-};
-
-function MessageText({ msg }: { msg: TMessage })
-{
-	const type = msg.getType();
-	const color = type in colors ? colors[type as keyof typeof colors] : "black";
-
-	if (["ai", "generic", "human"].includes(type) && msg.text.length > 0)
-	{
-		return <Text color={color}>{msg.text}</Text>;
-	}
-
-	return null;
-}
-
-function ToolCallDisplay({ toolCall }: { toolCall: ToolCall })
-{
-	return (
-		<Box borderStyle="single" borderColor="red" paddingX={1} width="100%">
-			<Text color="red">
-				Tool: {toolCall.name}({Object.entries(toolCall.args).map(([name, value]) =>
-					`${name}: ${value.toString().slice(0, 60)}`
-				).join(", ")})
-			</Text>
-		</Box>
-	);
-}
-
-function ToolCalls({ msg }: { msg: TMessage })
-{
-	const aiMessage: AIMessage | undefined = msg.getType() === "ai" ? msg : undefined;
-
-	if (!aiMessage?.tool_calls?.length)
-	{
-		return null;
-	}
-
-	return (
-		<Box flexDirection="column" width="100%">
-			{aiMessage?.tool_calls?.map((toolCall, index) => (
-				<ToolCallDisplay key={toolCall.id ?? index} toolCall={toolCall} />
-			))}
-		</Box>
-	);
-};
-
-const ToolMessageDisplay = ({ msg }: { msg: ToolMessage }) => (
-	<Text color={colors.tool}>
-		Tool: {msg.name}
-		<Newline />
-		{JSON.stringify(msg)}
-	</Text>
-);
-
-function Message({ msg, debug }: { msg: TMessage; debug?: boolean; })
-{
-	return (
-		<Box flexDirection="column" paddingBottom={1} width="100%">
-			<MessageText msg={msg} />
-			<ToolCalls msg={msg} />
-			{debug && msg.getType() === "tool" &&
-				<ToolMessageDisplay msg={msg as ToolMessage} />
-			}
-		</Box>
-	);
-}
-
-const MemoMessage = memo(Message);
+import MemoMessage, { Message } from "./ui/message.js";
+import Spinner from "./ui/spinner.js";
 
 interface ChatAppProps
 {

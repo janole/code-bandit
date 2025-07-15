40fcf967b4afa624d87b6dccf9469e3262d8d3ba
janole <ole@mobileways.de>
2025-07-15 22:04:24 +0200

    feat: enhance error handling by storing Error objects in ErrorMessage metadata

--- src/ai/error-message.ts
+++ src/ai/error-message.ts
@@ -12,11 +12,12 @@ class ErrorMessage extends BaseMessage
         return message.additional_kwargs["__coba_type"] === "error";
     }
 
-    constructor(content: string)
+    constructor(content: string, error?: Error)
     {
         super({ content });
 
         this.additional_kwargs["__coba_type"] = "error";
+        this.response_metadata = { error };
     }
 }
 

9fa1379207008c2d2bd10a5bee6e3469d8de0aac
janole <ole@mobileways.de>
2025-07-17 20:24:07 +0200

    refactor: simplify tool merging by removing intermediate object and merging directly into allTools

--- src/ai/tools/loader.ts
+++ src/ai/tools/loader.ts
@@ -1,12 +1,7 @@
 import { tools as commandExecutionTools } from "./command-execution-tools.js";
 import { tools as fileSystemTools } from "./file-system-tools.js";
 
-const tools = {
-    ...fileSystemTools,
-    ...commandExecutionTools,
-};
-
-const allTools = { ...tools };
+const allTools = { ...fileSystemTools, ...commandExecutionTools };
 const safeTools = Object.fromEntries(Object.entries(allTools).filter(([_, tool]) => !tool.metadata?.["destructive"]));
 
 interface GetToolsProps

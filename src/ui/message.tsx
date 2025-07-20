44bafc210b411ec62ef29cb643c799b0b79786ee
janole <ole@mobileways.de>
2025-07-20 14:34:03 +0200

    feat: refactor ToolMessageView and ErrorMessageView for improved structure and clarity

--- src/ui/message.tsx
+++ src/ui/message.tsx
@@ -1,9 +1,9 @@
-import { Badge, OrderedList, Spinner, StatusMessage, UnorderedList } from "@inkjs/ui";
-import { MessageType } from "@langchain/core/messages";
+import { Badge, Spinner, UnorderedList } from "@inkjs/ui";
+import { BaseMessage, MessageType } from "@langchain/core/messages";
 import { Box, Text } from "ink";
 import React, { memo } from "react";
 
-import { getCustomMessageType, ToolProgressMessage } from "../ai/messages.js";
+import { ErrorMessage, TMessageType, ToolProgressMessage } from "../ai/custom-messages.js";
 import { TMessage } from "../ai/work.js";
 import { Markdown } from "./markdown.js";
 
@@ -24,74 +24,93 @@ function MessageDebugLog({ msg }: { msg: TMessage })
     );
 }
 
-function ToolMessage({ msg }: { msg: TMessage })
+function ToolMessageView({ msg }: { msg: ToolProgressMessage })
 {
-    const items = (msg as ToolProgressMessage).items;
+    const items = msg.toolCalls;
 
     return (
-        <Box flexDirection="row" marginBottom={1} width={process.stdout.columns - 2}>
-            <Box>
-                <Badge color={colors.tool}>{"> TOOLS"}</Badge>
+        <Box width={process.stdout.columns - 2}>
+            <Box flexShrink={0} width={2}>
+                <Text color={colors.tool}>*</Text>
             </Box>
-            <Box paddingLeft={1}>
-                <OrderedList>
-                    {items.map((progress, index) => (
-                        <OrderedList.Item key={index}>
-                            <Box>
-                                {progress.status === "pending" ? <Spinner /> : <Text>{" "}</Text>}
+            <Box flexDirection="column" flexGrow={1} width={process.stdout.columns - 2 - 2}>
+                {items.map((progress, index) => (
+                    <Box key={index} marginBottom={1} flexDirection="column">
+                        <Box>
+                            <Text color={colors.tool}>
+                                {progress.toolCall.name}
+                            </Text>
 
-                                <StatusMessage variant={progress.status === "pending" ? "info" : progress.status}>
-                                    {progress.toolCall.name}
-                                </StatusMessage>
+                            {progress.status === "pending" &&
+                                <Spinner />
+                            }
 
-                                {progress.result &&
-                                    <Text color="gray">
-                                        {" → "}({progress.result?.content?.length})
-                                    </Text>
-                                }
+                            {progress.result &&
+                                <Text color="gray">
+                                    {" → "}
+
+                                    {progress.status === "error"
+                                        ? <Badge color="red">FAIL</Badge>
+                                        : <Badge color="green">OK</Badge>
+                                    }
+
+                                    {` (${progress.result?.content?.length})`}
+                                </Text>
+                            }
+                        </Box>
+                        <Box marginLeft={2}>
+                            <UnorderedList>
+                                {Object.entries(progress.toolCall?.args).map(([key, val]) => !!val && (
+                                    <UnorderedList.Item key={key}>
+                                        <Box width={process.stdout.columns / 2}>
+                                            <Text color="gray">{key}: </Text>
+                                            <Text color="blackBright">
+                                                {val.length > 40
+                                                    ? val.slice(0, 10) + " ... " + val.slice(-20)
+                                                    : val
+                                                }
+                                            </Text>
+                                        </Box>
+                                    </UnorderedList.Item>
+                                ))}
+                            </UnorderedList>
+                        </Box>
+                        {progress.status === "error" &&
+                            <Box marginLeft={2} borderStyle="double" borderColor={colors.error}>
+                                <Text>({progress.error?.message})</Text>
                             </Box>
-                            <Box paddingLeft={4}>
-                                <UnorderedList>
-                                    {Object.entries(progress.toolCall?.args).map(([key, val]) => !!val && (
-                                        <UnorderedList.Item key={key}>
-                                            <Box width={process.stdout.columns / 2}>
-                                                <Text color="gray">{key}: </Text>
-                                                <Text color="blackBright">
-                                                    {val.length > 40
-                                                        ? val.slice(0, 10) + " ... " + val.slice(-20)
-                                                        : val
-                                                    }
-                                                </Text>
-                                            </Box>
-                                        </UnorderedList.Item>
-                                    ))}
-                                </UnorderedList>
-                            </Box>
-                        </OrderedList.Item>
-                    ))}
-                </OrderedList>
+                        }
+                    </Box>
+                ))}
             </Box>
         </Box>
     );
 }
 
-function ErrorMessage({ msg: { text } }: { msg: TMessage })
+function ErrorMessageView({ msg: { content } }: { msg: ErrorMessage })
 {
     const color = colors.error;
 
     return (
-        <Box borderStyle="double" borderColor={color} paddingX={1}>
-            <Text color={color}>{text}</Text>
+        <Box borderStyle="double" borderColor={color} paddingX={1} marginBottom={1}>
+            <Text color={color}>{content}</Text>
         </Box>
     );
 }
 
-function TextMessage({ msg: { text }, type }: { msg: TMessage; type: MessageType })
+function BaseMessageView({ msg: { text }, type }: { msg: BaseMessage; type: TMessageType })
 {
+    const hasText = ["human", "ai", "generic"].includes(type) && text.trim().length > 0;
+
+    if (!hasText)
+    {
+        return null;
+    }
+
     const color = colors[type as keyof typeof colors];
 
     return (
-        <Box width={process.stdout.columns - 2}>
+        <Box width={process.stdout.columns - 2} marginBottom={1}>
             <Box width={2}>
                 <Text color={color}>
                     {type === "human"
@@ -112,36 +131,28 @@ function TextMessage({ msg: { text }, type }: { msg: TMessage; type: MessageType
 
 function Message({ msg, debug }: { msg: TMessage; debug?: boolean; })
 {
-    const type = getCustomMessageType(msg) || msg.getType();
+    const type = msg.getType();
 
     if (type === "tool-progress")
     {
-        return <ToolMessage msg={msg} />;
+        return <ToolMessageView msg={msg as ToolProgressMessage} />;
     }
 
     if (type === "error")
     {
-        return <ErrorMessage msg={msg} />;
+        return <ErrorMessageView msg={msg as ErrorMessage} />;
     }
 
-    const hasText = ["human", "ai", "generic"].includes(type) && msg.text.trim().length > 0;
+    return (<>
 
-    if (!debug && !hasText)
-    {
-        return null;
-    }
+        {(msg instanceof BaseMessage) &&
+            <BaseMessageView msg={msg} type={type as MessageType} />}
 
-    return (
-        <Box flexDirection="column" marginBottom={1} width={process.stdout.columns - 2}>
-            {hasText &&
-                <TextMessage type={type} msg={msg} />
-            }
+        {debug &&
+            <MessageDebugLog msg={msg} />
+        }
 
-            {debug &&
-                <MessageDebugLog msg={msg} />
-            }
-        </Box>
-    );
+    </>);
 }
 
 const MemoMessage = memo(Message);

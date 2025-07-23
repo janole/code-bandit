bc01a3b307d0d3f9bcb31ad5258f5f219f10eb98
janole <ole@mobileways.de>
2025-07-23 19:16:27 +0200

    build: replace manual externals with esbuild-node-externals plugin and add plugin dependency

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,10 +1,5 @@
 import esbuild from "esbuild";
-import fs from "fs";
-import { builtinModules } from "module";
-
-// Read dependencies from package.json
-const packageJson = JSON.parse(fs.readFileSync("package.json", "utf-8"));
-const deps = Object.keys(packageJson.dependencies || {});
+import { nodeExternalsPlugin } from "esbuild-node-externals";
 
 const buildOptions = {
     bundle: true,
@@ -17,23 +12,17 @@ const buildOptions = {
 await Promise.all([
     esbuild.build({
         ...buildOptions,
-        // Mark everything external except '@langchain/ollama'
-        external: [
-            ...builtinModules,
-            ...deps.filter(dep => dep !== "@langchain/ollama"),
-        ],
         entryPoints: ["src/coba.tsx"],
         outfile: "dist/coba.js",
+        plugins: [nodeExternalsPlugin({
+            allowList: ["@langchain/ollama"],
+        })],
     }),
     esbuild.build({
         ...buildOptions,
-        // Mark everything external
-        external: [
-            ...builtinModules,
-            ...deps,
-        ],
         entryPoints: ["src/index.ts"],
         outfile: "dist/index.js",
+        plugins: [nodeExternalsPlugin()],
     })
 ]);
 

5dcd07e023ce9c122f6a64f900840de6cf49c543
janole <ole@mobileways.de>
2025-08-03 18:36:25 +0200

    refactor: update ChatSession export path to use session module

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export { ChatSession, type IChatSession } from "./ai/chat-session.js";
 export { type TMessage } from "./ai/custom-messages.js";
+export { ChatSession, type IChatSession } from "./ai/session/session.js";
 export { work } from "./ai/work.js";
 export { AIMessage, AIMessageChunk, BaseMessage, HumanMessage, ToolMessage, ToolMessageChunk } from "@langchain/core/messages";
